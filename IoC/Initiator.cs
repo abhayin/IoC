a91aaaf254d33a55ae0c3531afaee3fa22c788c0
Abhay <abhay.in@live.com>
2017-03-12 23:48:28 +0530

    IoC sample

---
+++ IoC/Initiator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC
+{
+    public class Initiator
+    {
+        internal static readonly IUnityContainer unity = new UnityContainer();
+
+        public Initiator()
+        {
+            unity.RegisterInstance<ILogger>(new Logger());
+            unity.Resolve<Logger>();
+        }
+
+
+
+    }
+}
---
+++ IoC/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC
+{
+    public class Logger : ILogger
+    {
+        public string ReturnString()
+        {
+            return "Logger";
+        }
+    }
+}
